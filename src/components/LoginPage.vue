4ff0761dae569b71e94babb622052c2a98ebe0c6
SerhiiSemennikov <semennik@hotmail.com>
2025-04-21 16:38:13 +0200

    copy1

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,94 @@
+<script>
+import { addUser, getUserByEmail } from "../api/users.js";
+import NeedToRegister from "./needToRegister.vue";
+export default {
+  name: "LoginPage",
+  components: {
+    NeedToRegister,
+  },
+  data() {
+    return {
+      email: "",
+      // name: "",
+      isNeedToRegister: false,
+    };
+  },
+  emits: ["addUser", "name"],
+  methods: {
+    handleLogin(email) {
+      getUserByEmail(email).then(({ data }) => {
+        if (!data.length) {
+          this.isNeedToRegister = true;
+        } else {
+          console.log(data);
+          localStorage.setItem("user", JSON.stringify(data[0]));
+          this.$emit("addUser", data);
+        }
+      });
+    },
+    handleRegister() {
+      const name = this.name;
+      console.log("name", name);
+      const email = this.email;
+      const username = this.username || null;
+      const phone = this.phone || null;
+      addUser({ name, username, email, phone });
+      this.handleLogin(email);
+    },
+  },
+};
+</script>
+
+<template>
+  <section class="container is-flex is-justify-content-center">
+    <form
+      @submit.prevent="
+        isNeedToRegister ? handleRegister() : handleLogin(email)
+      "
+      class="box mt-5"
+    >
+      <h1 class="title is-3">
+        {{ isNeedToRegister ? "You need to register" : "Get your userId" }}
+      </h1>
+
+      <div class="field">
+        <label class="label" htmlFor="user-email"> Email </label>
+
+        <div class="control has-icons-left">
+          <input
+            v-model="email"
+            type="email"
+            id="user-email"
+            name="email"
+            class="input"
+            placeholder="Enter your email"
+            required
+            :disabled="isNeedToRegister"
+          />
+
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope" />
+          </span>
+        </div>
+      </div>
+
+      <div v-if="isNeedToRegister" class="field">
+
+     <NeedToRegister #default="{ x }" >
+          {{ (name = x) }}
+        </NeedToRegister>
+     
+
+  
+      </div>
+
+      <div class="field">
+        <button type="submit" class="button is-primary">
+          {{ isNeedToRegister ? "Register" : "Login" }}
+        </button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script></script>
---
+++ src/components/needToRegister.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: "NeedToRegister",
+  data() {
+    return {
+      name: "",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="field">
+    <label class="label" htmlFor="user-name"> Your Name </label>
+
+    <div class="control has-icons-left">
+      <input
+        v-model="name"
+        type="text"
+        id="user-name"
+        name="name"
+        class="input"
+        placeholder="Enter your name"
+        required
+        minlength="{4}"
+      />
+      <div class="body">
+        <slot :x="name">{{ name }}</slot>
+      </div>
+      <span class="icon is-small is-left">
+        <i class="fas fa-user" />
+      </span>
+    </div>
+
+    <p v-if="!name" class="help is-danger">Start type Your Name please!</p>
+  </div>
+</template>
+
+<style></style>
